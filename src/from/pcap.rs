c39078aa5eeb664904d89daaa87a11fdd8c8cbb0
banst <bastien@loftorbital.com>
2025-01-08 23:34:57 +0100

    feat: improve robustness of file parsing

--- src/from/pcap.rs
+++ src/from/pcap.rs
@@ -5,9 +5,8 @@ use nu_plugin::{EngineInterface, EvaluatedCall, SimplePluginCommand};
 use nu_protocol::{Category, LabeledError, Record, Signature, Type, Value};
 use pcap_parser::data::PacketData;
 use pcap_parser::traits::PcapReaderIterator;
-use pcap_parser::{LegacyPcapReader, PcapBlockOwned, PcapError};
+use pcap_parser::{LegacyPcapReader, Linktype, PcapBlockOwned, PcapError};
 use std::io::Cursor;
-use std::net::IpAddr;
 
 pub struct FromPcap;
 
@@ -41,19 +40,20 @@ impl SimplePluginCommand for FromPcap {
         let cursor = Cursor::new(buffer);
         let mut reader = LegacyPcapReader::new(65536, cursor).expect("LegacyPcapReader");
         let mut records = Vec::new();
-        let mut num_blocks = 0;
+        let mut num_packets = 0;
+        let mut network = Option::<Linktype>::None;
 
         loop {
             match reader.next() {
                 Ok((offset, block)) => {
-                    num_blocks += 1;
                     match block {
-                        PcapBlockOwned::LegacyHeader(_hdr) => {
-                            // TODO: capture header to use later in pcap_parser::data
+                        PcapBlockOwned::LegacyHeader(hdr) => {
+                            network = Some(hdr.network);
                         }
                         PcapBlockOwned::Legacy(block) => {
+                            num_packets += 1;
                             let mut packet_record = Record::new();
-                            packet_record.push("number", Value::int(num_blocks, span));
+                            packet_record.push("number", Value::int(num_packets, span));
                             packet_record.push(
                                 "timestamp",
                                 Value::float(
@@ -61,40 +61,23 @@ impl SimplePluginCommand for FromPcap {
                                     span,
                                 ),
                             );
-                            packet_record
-                                .push("captured_length", Value::int(block.caplen.into(), span));
-                            packet_record
-                                .push("original_length", Value::int(block.origlen.into(), span));
+                            packet_record.push("length", Value::int(block.origlen.into(), span));
 
-                            let packet_data = pcap_parser::data::get_packetdata_null(
-                                &block.data,
-                                block.caplen as usize,
-                            )
-                            .unwrap();
+                            let maybe_packet_data = match network {
+                                Some(linktype) => pcap_parser::data::get_packetdata(
+                                    &block.data,
+                                    linktype,
+                                    block.caplen as usize,
+                                ),
+                                None => pcap_parser::data::get_packetdata_ethernet(
+                                    &block.data,
+                                    block.caplen as usize,
+                                ),
+                            };
 
-                            match PacketInfo::try_from(packet_data) {
-                                Ok(packet_info) => {
-                                    packet_record.push(
-                                        "source",
-                                        Value::string(packet_info.source.to_string(), span),
-                                    );
-                                    packet_record.push(
-                                        "destination",
-                                        Value::string(packet_info.destination.to_string(), span),
-                                    );
-                                    packet_record.push(
-                                        "protocol",
-                                        Value::string(packet_info.protocol, span),
-                                    );
-                                    packet_record
-                                        .push("details", Value::string(packet_info.details, span));
-                                    packet_record
-                                        .push("payload", Value::binary(packet_info.payload, span));
-                                }
-                                Err(err) => {
-                                    packet_record
-                                        .push("details", Value::string(err.to_string(), span));
-                                }
+                            // If we do have packet data, push it to the record
+                            if let Some(packet_data) = maybe_packet_data {
+                                write_packet_data_in(&packet_data, &mut packet_record, span);
                             }
 
                             records.push(Value::record(packet_record, span));
@@ -118,82 +101,78 @@ impl SimplePluginCommand for FromPcap {
     }
 }
 
-#[derive(Debug)]
-enum PacketParseError {
-    Unsupported,
-    ParseError(String),
-}
-
-impl std::fmt::Display for PacketParseError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            PacketParseError::Unsupported => write!(f, "Unsupported packet type"),
-            PacketParseError::ParseError(msg) => write!(f, "Parse error: {}", msg),
-        }
-    }
-}
-
-struct PacketInfo {
-    source: IpAddr,
-    destination: IpAddr,
-    protocol: String,
-    details: String,
-    payload: Vec<u8>,
-}
-
-impl TryFrom<PacketData<'_>> for PacketInfo {
-    type Error = PacketParseError;
-
-    fn try_from(packet_data: PacketData<'_>) -> Result<Self, Self::Error> {
-        let packet = match packet_data {
-            PacketData::L2(l2) => SlicedPacket::from_ethernet(l2).map_err(|e| {
-                PacketParseError::ParseError(format!("Error parsing layer 2 packet: {}", e))
-            })?,
-            PacketData::L3(_, l3) => SlicedPacket::from_ip(l3).map_err(|e| {
-                PacketParseError::ParseError(format!("Error parsing layer 3 packet: {}", e))
-            })?,
-            _ => return Err(PacketParseError::Unsupported),
-        };
-
-        let (src, dst) = match packet.net {
-            Some(etherparse::InternetSlice::Ipv4(ipv4)) => (
-                IpAddr::V4(ipv4.header().source_addr()),
-                IpAddr::V4(ipv4.header().destination_addr()),
-            ),
-            Some(etherparse::InternetSlice::Ipv6(ipv6)) => (
-                IpAddr::V6(ipv6.header().source_addr()),
-                IpAddr::V6(ipv6.header().destination_addr()),
-            ),
-            _ => (
-                IpAddr::V4(std::net::Ipv4Addr::new(0, 0, 0, 0)),
-                IpAddr::V4(std::net::Ipv4Addr::new(0, 0, 0, 0)),
-            ),
-        };
-
-        match packet.transport {
-            Some(etherparse::TransportSlice::Udp(udp)) => {
-                Ok(PacketInfo {
-                    source: src, //destination: packet.ip.destination(),
-                    destination: dst,
-                    protocol: "UDP".to_string(),
-                    details: format!("{} -> {}", udp.source_port(), udp.destination_port()),
-                    payload: udp.payload().to_vec(),
-                })
+fn write_packet_data_in(
+    packet_data: &PacketData<'_>,
+    packet_record: &mut Record,
+    span: nu_protocol::Span,
+) {
+    let packet = match packet_data {
+        PacketData::L2(l2) => match SlicedPacket::from_ethernet(l2) {
+            Ok(packet) => packet,
+            Err(e) => {
+                packet_record.push("details", Value::string(e.to_string(), span));
+                return;
             }
-            Some(etherparse::TransportSlice::Tcp(tcp)) => Ok(PacketInfo {
-                source: src,
-                destination: dst,
-                protocol: "TCP".to_string(),
-                details: format!("{} -> {}", tcp.source_port(), tcp.destination_port()),
-                payload: tcp.payload().to_vec(),
-            }),
-            _ => Ok(PacketInfo {
-                source: src,
-                destination: dst,
-                protocol: "Unknown".to_string(),
-                details: "Unknown".to_string(),
-                payload: Vec::new(),
-            }),
+        },
+        PacketData::L3(_, l3) => match SlicedPacket::from_ip(l3) {
+            Ok(packet) => packet,
+            Err(e) => {
+                packet_record.push("details", Value::string(e.to_string(), span));
+                return;
+            }
+        },
+        _ => {
+            packet_record.push("details", Value::string("Unsupported packet type", span));
+            return;
+        }
+    };
+
+    match packet.net {
+        Some(etherparse::InternetSlice::Ipv4(ipv4)) => {
+            packet_record.push(
+                "source",
+                Value::string(ipv4.header().source_addr().to_string(), span),
+            );
+            packet_record.push(
+                "destination",
+                Value::string(ipv4.header().destination_addr().to_string(), span),
+            );
+        }
+        Some(etherparse::InternetSlice::Ipv6(ipv6)) => {
+            packet_record.push(
+                "source",
+                Value::string(ipv6.header().source_addr().to_string(), span),
+            );
+            packet_record.push(
+                "destination",
+                Value::string(ipv6.header().destination_addr().to_string(), span),
+            );
+        }
+        _ => {
+            packet_record.push("source", Value::string("unknown", span));
+            packet_record.push("destination", Value::string("unknown", span));
+        }
+    };
+
+    match packet.transport {
+        Some(etherparse::TransportSlice::Udp(udp)) => {
+            packet_record.push("protocol", Value::string("UDP", span));
+            packet_record.push("src_port", Value::int(udp.source_port() as i64, span));
+            packet_record.push("dst_port", Value::int(udp.destination_port() as i64, span));
+        }
+        Some(etherparse::TransportSlice::Tcp(tcp)) => {
+            packet_record.push("protocol", Value::string("TCP", span));
+            packet_record.push("src_port", Value::int(tcp.source_port() as i64, span));
+            packet_record.push("dst_port", Value::int(tcp.destination_port() as i64, span));
+        }
+        Some(etherparse::TransportSlice::Icmpv4(_)) => {
+            packet_record.push("protocol", Value::string("ICMPv4", span));
+        }
+        Some(etherparse::TransportSlice::Icmpv6(_)) => {
+            packet_record.push("protocol", Value::string("ICMPv6", span));
+        }
+        _ => {
+            packet_record.push("protocol", Value::string("Unknown", span));
         }
     }
 }
