fe139fa02065b7f6e000335197f8643c194f3efb
banst <bastien@loftorbital.com>
2025-01-09 18:11:26 +0100

    feat: implement stream support

--- src/from/pcap.rs
+++ src/from/pcap.rs
@@ -1,16 +1,20 @@
 use crate::FormatPcapPlugin;
 
 use etherparse::SlicedPacket;
-use nu_plugin::{EngineInterface, EvaluatedCall, SimplePluginCommand};
-use nu_protocol::{Category, LabeledError, Record, Signature, Type, Value};
+use nu_plugin::*;
+use nu_protocol::byte_stream::{ByteStream, Reader};
+use nu_protocol::IntoInterruptiblePipelineData;
+use nu_protocol::{Category, LabeledError, Record, Signature, Type};
+use nu_protocol::{PipelineData, Signals, Span, Value};
 use pcap_parser::data::PacketData;
 use pcap_parser::traits::PcapReaderIterator;
 use pcap_parser::{LegacyPcapReader, Linktype, PcapBlockOwned, PcapError};
-use std::io::Cursor;
+use std::sync::mpsc;
+use std::thread;
 
 pub struct FromPcap;
 
-impl SimplePluginCommand for FromPcap {
+impl PluginCommand for FromPcap {
     type Plugin = FormatPcapPlugin;
 
     fn name(&self) -> &str {
@@ -32,73 +36,111 @@ impl SimplePluginCommand for FromPcap {
         _plugin: &FormatPcapPlugin,
         _engine: &EngineInterface,
         _call: &EvaluatedCall,
-        input: &Value,
-    ) -> Result<Value, LabeledError> {
-        let span = input.span();
-        let buffer = input.as_binary()?;
+        input: PipelineData,
+    ) -> Result<PipelineData, LabeledError> {
+        let span = input.span().expect("span is missing");
 
-        let cursor = Cursor::new(buffer);
-        let mut reader = LegacyPcapReader::new(65536, cursor).expect("LegacyPcapReader");
-        let mut records = Vec::new();
-        let mut num_packets = 0;
-        let mut network = Option::<Linktype>::None;
+        // Create a pcap reader from the input ByteStream
+        let byte_stream = extract_byte_stream(input, span)?;
+        let bs_reader = create_byte_stream_reader(byte_stream, span)?;
+        let mut reader = create_pcap_reader(bs_reader, span)?;
 
-        loop {
-            match reader.next() {
-                Ok((offset, block)) => {
-                    match block {
-                        PcapBlockOwned::LegacyHeader(hdr) => {
-                            network = Some(hdr.network);
-                        }
-                        PcapBlockOwned::Legacy(block) => {
-                            num_packets += 1;
-                            let mut packet_record = Record::new();
-                            packet_record.push("number", Value::int(num_packets, span));
-                            packet_record.push(
-                                "timestamp",
-                                Value::float(
-                                    block.ts_sec as f64 + block.ts_usec as f64 / 1_000_000.0,
-                                    span,
-                                ),
-                            );
-                            packet_record.push("length", Value::int(block.origlen.into(), span));
+        // Spawn a thread to process the ByteStream
+        let (tx, rx) = mpsc::channel();
+        thread::spawn(move || {
+            // Create a pcap reader from the cursor
+            let mut num_packets = 0;
+            let mut network = Option::<Linktype>::None;
 
-                            let maybe_packet_data = match network {
-                                Some(linktype) => pcap_parser::data::get_packetdata(
-                                    &block.data,
-                                    linktype,
-                                    block.caplen as usize,
-                                ),
-                                None => pcap_parser::data::get_packetdata_ethernet(
-                                    &block.data,
-                                    block.caplen as usize,
-                                ),
-                            };
-
-                            // If we do have packet data, push it to the record
-                            if let Some(packet_data) = maybe_packet_data {
-                                write_packet_data_in(&packet_data, &mut packet_record, span);
+            // Iterate over the pcap blocks
+            loop {
+                match reader.next() {
+                    Ok((offset, block)) => {
+                        match block {
+                            PcapBlockOwned::LegacyHeader(hdr) => {
+                                network = Some(hdr.network);
                             }
+                            PcapBlockOwned::Legacy(block) => {
+                                num_packets += 1;
+                                let mut packet_record = Record::new();
+                                packet_record.push("number", Value::int(num_packets, span));
+                                packet_record.push(
+                                    "timestamp",
+                                    Value::float(
+                                        block.ts_sec as f64 + block.ts_usec as f64 / 1_000_000.0,
+                                        span,
+                                    ),
+                                );
+                                packet_record
+                                    .push("length", Value::int(block.origlen.into(), span));
 
-                            records.push(Value::record(packet_record, span));
+                                let maybe_packet_data = match network {
+                                    Some(linktype) => pcap_parser::data::get_packetdata(
+                                        &block.data,
+                                        linktype,
+                                        block.caplen as usize,
+                                    ),
+                                    None => pcap_parser::data::get_packetdata_ethernet(
+                                        &block.data,
+                                        block.caplen as usize,
+                                    ),
+                                };
+
+                                // If we do have packet data, push it to the record
+                                if let Some(packet_data) = maybe_packet_data {
+                                    write_packet_data_in(&packet_data, &mut packet_record, span);
+                                }
+
+                                // If send fails, the receiving end has been dropped. We stop processing
+                                if tx.send(Value::record(packet_record, span)).is_err() {
+                                    break;
+                                }
+                            }
+                            PcapBlockOwned::NG(_) => unreachable!(),
                         }
-                        PcapBlockOwned::NG(_) => unreachable!(),
+                        reader.consume(offset);
                     }
-                    reader.consume(offset);
-                }
-                Err(PcapError::Eof) => break,
-                Err(PcapError::Incomplete(_)) => {
-                    reader.refill().unwrap();
-                }
-                Err(e) => {
-                    return Err(LabeledError::new("Error parsing pcap file")
-                        .with_label(e.to_string(), span))
+                    Err(PcapError::Eof) => break,
+                    Err(PcapError::Incomplete(_)) => {
+                        reader.refill().unwrap();
+                    }
+                    Err(e) => {
+                        eprintln!("Error: {:?}", e);
+                    }
                 }
             }
-        }
+        });
 
-        Ok(Value::list(records, span))
+        // Wrap the record stream in a ListStream and return it
+        Ok(rx.into_iter().into_pipeline_data(span, Signals::empty()))
     }
+}
+
+fn extract_byte_stream(input: PipelineData, span: Span) -> Result<ByteStream, LabeledError> {
+    match input {
+        PipelineData::ByteStream(byte_stream, _) => Ok(byte_stream),
+        _ => Err(LabeledError::new("Expected a ByteStream as input")
+            .with_label("Input was not a ByteStream", span)),
+    }
+}
+
+fn create_byte_stream_reader(byte_stream: ByteStream, span: Span) -> Result<Reader, LabeledError> {
+    byte_stream
+        .reader()
+        .ok_or("Failed to get reader from ByteStream")
+        .map_err(|e| {
+            LabeledError::new("Failed to create a byte stream reader")
+                .with_label(e.to_string(), span)
+        })
+}
+
+fn create_pcap_reader(
+    reader: Reader,
+    span: Span,
+) -> Result<LegacyPcapReader<Reader>, LabeledError> {
+    LegacyPcapReader::new(65536, reader).map_err(|e| {
+        LabeledError::new("Failed to create a pcap reader").with_label(e.to_string(), span)
+    })
 }
 
 fn write_packet_data_in(
